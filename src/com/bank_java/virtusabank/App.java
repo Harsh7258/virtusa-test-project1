012d2dca3be2e792aaa9ed25f0657c742d975899
K Harsh <harshnaidu2u@gmail.com>
2025-11-27 16:35:48 +0530

    Bank project: inactive acc validation

---
+++ src/com/bank_java/virtusabank/App.java
@@ -0,0 +1,9 @@
+package com.java.virtusabank;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/com/bank_java/virtusabank/dao/BankDao.java
@@ -0,0 +1,14 @@
+package com.bank_java.virtusabank.dao;
+
+import com.bank_java.virtusabank.model.Accounts;
+
+import java.sql.SQLException;
+
+public interface BankDao {
+  String createAccount(Accounts account) throws SQLException, ClassNotFoundException;
+  Accounts searchAccount(int accountNo) throws SQLException, ClassNotFoundException;
+  String updateAccount(int accountNo, String city, String state) throws SQLException, ClassNotFoundException;
+  String closeAccount(int accountNo) throws SQLException, ClassNotFoundException;
+  String depositAccount(int accountNo, double amount) throws SQLException, ClassNotFoundException;
+  String withdrawAccount(int accountNo, double amount) throws SQLException, ClassNotFoundException;
+}
